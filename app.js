09222b4ac8cd0b18986624c74cbcdfe7f7b964fc
S. Rohit <rohit.shivaji.shinde@gmail.com>
2017-11-28 14:49:10 +0530

    Changes in app.js for better understanding of bot

--- app.js
+++ app.js
@@ -9,14 +9,73 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: '',
-    appPassword: '',
+    appId: 'cd2fb699-1663-45a3-a1cf-ab4cd22623c2',
+    appPassword: 'ysmplMXQ0847*)sfUUPF6$='
 });
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-})
+var bot = new builder.UniversalBot(connector);
+
+bot.dialog('/', function (session, args) {
+    if (!session.userData.greeting) {
+        session.send("Hello. What is your name?");
+        session.userData.greeting = true;
+    } else if (!session.userData.name) {
+        getName(session);
+    } else if (!session.userData.email) {
+        getEmail(session);
+    } else if (!session.userData.password) {
+        getPassword(session);
+    } else {
+        session.userData = null;
+    }
+    session.endDialog();
+});
+function getName(session) {
+    name = session.message.text;
+    session.userData.name = name;
+    session.send("Hello, " + name + ". What is your Email ID?");
+}
+function getEmail(session) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    email = session.message.text;
+    if (re.test(email)) {
+        session.userData.email = email;
+        session.send("Thank you, " + session.userData.name + ". Please set a new password.");
+    } else {
+        session.send("Please type a valid email address. For example: test@hotmail.com");
+    }
+}
+function getPassword(session) {
+    var re = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    password = session.message.text;
+    if (re.test(password)) {
+        session.userData.password = password;
+        var data = session.userData;
+        sendData(data, function (msg) {
+            session.send(msg);
+            session.userData = null;
+        });
+    } else {
+        session.send("Password must contain at least 8 characters, including at least 1 number, 1 uppercase letter, 1 lower case letter and 1 special character. For example: Mybot@123");
+    }
+}
+
+function sendData(data, cb) {
+    http.get("http://local.dev/github/troublehacker/Emulator/data.php?name=" + data.name + "&email=" + data.email + "&password=" + data.password, function (res) {
+        var msg = '';
+        res.on("data", function (chunk) {
+            msg += chunk;
+        });
+
+        res.on('end', function () {
+            cb(msg);
+        });
+
+    }).on('error', function (e) {
+        console.log("Got error: " + e.message);
+    });
+}
